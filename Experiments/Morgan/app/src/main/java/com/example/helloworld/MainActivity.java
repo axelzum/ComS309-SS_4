0fa950639024aaa4bf17e5667a808bddfd745f23
msmith <msmith@iastate.edu>
2019-09-10 10:49:31 -0500

    Hello World2

--- Experiments/Morgan/app/src/main/java/com/example/helloworld/MainActivity.java
+++ Experiments/Morgan/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,4 +1,18 @@
 package com.example.helloworld;
 
-public class MainActivity {
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.helloworld.R;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+    }
 }
