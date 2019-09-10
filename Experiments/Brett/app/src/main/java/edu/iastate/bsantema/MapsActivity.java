f9e10346706f31ac971b462d2c90d32e67b1db56
bsantema <bsantema@iastate.edu>
2019-09-10 02:04:29 -0500

    Experimenting with map boundaries and Polylines

--- Experiments/Brett/app/src/main/java/edu/iastate/bsantema/MapsActivity.java
+++ Experiments/Brett/app/src/main/java/edu/iastate/bsantema/MapsActivity.java
@@ -1,17 +1,20 @@
 package edu.iastate.bsantema;
 
-import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnPolylineClickListener {
 
     private GoogleMap mMap;
 
@@ -39,9 +42,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Ames and move the camera
+        LatLng ames = new LatLng(42.025821, -93.646444);
+        mMap.addMarker(new MarkerOptions().position(ames).title("Marker in Ames"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ames));
+
+        // Set proper zoom levels and map boundaries
+        mMap.setMinZoomPreference(15);
+        LatLng isuSW = new LatLng(42.001631, -93.658071);
+        LatLng isuNE = new LatLng(42.039406, -93.625058);
+        LatLngBounds isuBoundry = new LatLngBounds(isuSW, isuNE);
+        mMap.setLatLngBoundsForCameraTarget(isuBoundry);
+
+        // Create Polyline around central campus
+        Polyline polyline1 = googleMap.addPolyline((new PolylineOptions())
+                .clickable(true)
+                .add(new LatLng(42.027013, -93.647404),
+                        new LatLng(42.027292, -93.646256),
+                        new LatLng(42.026917, -93.644776),
+                        new LatLng(42.025801, -93.644626),
+                        new LatLng(42.025020, -93.645849),
+                        new LatLng(42.025777, -93.647394),
+                        new LatLng(42.027013, -93.647404)));
+
+        mMap.setOnPolylineClickListener(this);
+        polyline1.setWidth(20);
+    }
+
+    @Override
+    public void onPolylineClick(Polyline polyline) { //Alternates Color
+        if(polyline.getColor()!=0xff00ffff)
+            polyline.setColor(0xff00ffff);
+        else
+            polyline.setColor(0xff000000);
     }
 }
