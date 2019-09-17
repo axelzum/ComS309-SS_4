fd2bdf3b614cd741b7db00248dca89627f006347
bsantema <bsantema@iastate.edu>
2019-09-17 00:25:40 -0500

    Added custom styles, custom marker icon/interaction

--- Experiments/Brett/app/src/main/java/edu/iastate/bsantema/MapsActivity.java
+++ Experiments/Brett/app/src/main/java/edu/iastate/bsantema/MapsActivity.java
@@ -10,13 +10,17 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnPolylineClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnPolylineClickListener, GoogleMap.OnMarkerDragListener {
 
     private GoogleMap mMap;
+    private float markerRotation = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,21 +44,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.style_opencampus));
         mMap = googleMap;
 
-        // Add a marker in Ames and move the camera
+        // Add a marker in Sydney and move the camera
         LatLng ames = new LatLng(42.025821, -93.646444);
-        mMap.addMarker(new MarkerOptions().position(ames).title("Marker in Ames"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(ames));
+        mMap.addMarker(new MarkerOptions()
+                .position(ames)
+                .title("Marker in Ames")
+                .draggable(true)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_scribble)));
 
-        // Set proper zoom levels and map boundaries
+        mMap.setOnMarkerDragListener(this);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ames));
         mMap.setMinZoomPreference(15);
         LatLng isuSW = new LatLng(42.001631, -93.658071);
         LatLng isuNE = new LatLng(42.039406, -93.625058);
         LatLngBounds isuBoundry = new LatLngBounds(isuSW, isuNE);
         mMap.setLatLngBoundsForCameraTarget(isuBoundry);
 
-        // Create Polyline around central campus
         Polyline polyline1 = googleMap.addPolyline((new PolylineOptions())
                 .clickable(true)
                 .add(new LatLng(42.027013, -93.647404),
@@ -70,10 +79,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
-    public void onPolylineClick(Polyline polyline) { //Alternates Color
+    public void onPolylineClick(Polyline polyline) {
         if(polyline.getColor()!=0xff00ffff)
             polyline.setColor(0xff00ffff);
         else
             polyline.setColor(0xff000000);
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker)
+    {
+        marker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.marker_scribble_highlighted));
+        marker.setAlpha((float)0.5);
+        marker.setSnippet("Moving Marker...");
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker)
+    {
+        marker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.marker_scribble));
+        marker.setSnippet("");
+        marker.setAlpha((float)1.0);
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker)
+    {
+        markerRotation += 6;
+        marker.setRotation(markerRotation);
     }
 }
