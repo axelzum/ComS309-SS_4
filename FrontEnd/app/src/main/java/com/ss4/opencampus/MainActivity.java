3e07c4bb72d728840a844c4c81bfd9d26a75e280
bsantema <bsantema@iastate.edu>
2019-09-25 20:20:35 -0500

    Added ability to open Maps Activity from Main Activity with a button.

--- FrontEnd/app/src/main/java/com/ss4/opencampus/MainActivity.java
+++ FrontEnd/app/src/main/java/com/ss4/opencampus/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ss4.opencampus;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.container,MapsActivity.class,YOUR_FRAGMENT_STRING_TAG);
+        transaction.addToBackStack(null);
+        transaction.commit();
+        */
     }
+
+    public void viewMapsActivity(View view)
+    {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
 }
