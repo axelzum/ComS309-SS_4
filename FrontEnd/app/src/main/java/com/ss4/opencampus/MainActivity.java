0a0bcb3a99c1b7c90b2c74e40e78c00040b6d345
Axel Zumwalt <axelzum@iastate.edu>
2019-09-30 21:40:32 -0500

    [SS4-14] simple login auth, and create account

--- FrontEnd/app/src/main/java/com/ss4/opencampus/MainActivity.java
+++ FrontEnd/app/src/main/java/com/ss4/opencampus/MainActivity.java
@@ -5,14 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "tag";
+
+    private RequestQueue queue;
+
+    private EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = (EditText)findViewById(R.id.editText4);
     }
+
     public void viewRegister(View view)
     {
         Intent intent = new Intent(this, Register.class);
@@ -21,7 +43,46 @@ public class MainActivity extends AppCompatActivity {
 
     public void viewDashboard(View view)
     {
-        Intent intent = new Intent(this, Dashboard.class);
-        startActivity(intent);
+        queue = Volley.newRequestQueue(this);
+        String url = "http://coms-309-ss-4.misc.iastate.edu:8080/students/search/all";
+
+        // Request a JSONObject response from the provided URL.
+        JsonArrayRequest jsonRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject student = response.getJSONObject(i);
+                                if (student.getString("email").equals(email.getText().toString())) {
+                                    Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                                    startActivity(intent);
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        //Set the tag on the request
+        jsonRequest.setTag(TAG);
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonRequest);
+
+    }
+
+    @Override
+    protected void onStop () {
+        super.onStop();
+        if (queue != null) {
+            queue.cancelAll(TAG);
+        }
     }
 }
--- FrontEnd/app/src/main/java/com/ss4/opencampus/Register.java
+++ FrontEnd/app/src/main/java/com/ss4/opencampus/Register.java
@@ -5,18 +5,105 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+
+    private static final String TAG = "tag";
+
+    private EditText firstName;
+
+    private EditText lastName;
+
+    private EditText userName;
+
+    private EditText email;
+
+    private EditText password;
+
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        firstName = (EditText) findViewById(R.id.editText);
+        lastName = (EditText) findViewById(R.id.editText2);
+        userName = (EditText) findViewById(R.id.editText3);
+        email = (EditText) findViewById(R.id.editText4);
+        password = (EditText) findViewById(R.id.editText5);
     }
 
-    public void viewDashboard(View view)
-    {
-        Intent intent = new Intent(this, Dashboard.class);
-        startActivity(intent);
+    public void addStudent(View view) {
+        JSONObject newStudent = new JSONObject();
+        try {
+            newStudent.put("firstName", firstName.getText());
+            newStudent.put("lastName", lastName.getText());
+            newStudent.put("userName", userName.getText());
+            newStudent.put("email", email.getText());
+            newStudent.put("password", password.getText());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        queue = Volley.newRequestQueue(this);
+        String url = "http://coms-309-ss-4.misc.iastate.edu:8080/students/add";
+
+        // Request a JSON response from the provided URL.
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, newStudent, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                //postResp.setText("win");
+                try {
+                    firstName.setText(response.get("response").toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                return headers;
+            }
+        };
+
+        //Set the tag on the request
+        jsonRequest.setTag(TAG);
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonRequest);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (queue != null) {
+            queue.cancelAll(TAG);
+        }
     }
 }
+
+
