adbf0c840e1006cfc5c12b8a953833f9a96a9c0d
Willis Knox <wpknox@iastate.edu>
2019-10-04 12:01:16 -0500

    [SS4-18] Refactor Endpoints. POSTs now return Boolean and GETs are compiled into larger methods to make additions easier

--- BackEnd/database/src/main/java/com/ss4/opencampus/backend/database/buildings/BuildingController.java
+++ BackEnd/database/src/main/java/com/ss4/opencampus/backend/database/buildings/BuildingController.java
@@ -35,7 +35,7 @@ public class BuildingController
    */
   @PostMapping(path = "/add")
   public @ResponseBody
-  Map<String, String> addSingleBuilding(@RequestBody Building building)
+  Map<String, Boolean> addSingleBuilding(@RequestBody Building building)
   {
     try
     {
@@ -43,9 +43,9 @@ public class BuildingController
     }
     catch (Exception e)
     {
-      return Collections.singletonMap("response", "Failed to add building to Buildings table");
+      return Collections.singletonMap("response", false);
     }
-    return Collections.singletonMap("response", "New building added to Buildings table");
+    return Collections.singletonMap("response", true);
   }
 
   /**
@@ -59,7 +59,7 @@ public class BuildingController
    */
   @PostMapping(path = "/addMultiple")
   public @ResponseBody
-  Map<String, String> addMultipleBuildings(@RequestBody Building[] buildings)
+  Map<String, Boolean> addMultipleBuildings(@RequestBody Building[] buildings)
   {
     int addedCnt = 0;
     try
@@ -72,102 +72,80 @@ public class BuildingController
     }
     catch (Exception e)
     {
-      return Collections.singletonMap("response","Ran into error. Only added " + addedCnt + " building(s) to the Buildings table" );
+      return Collections.singletonMap("response", false);
     }
-    return Collections.singletonMap("response", "Added " + addedCnt + " buildings to the Buildings table.");
+    return Collections.singletonMap("response", true);
   }
 
   /**
-   * Get a list of all buildings in the database
+   * Mapping for the different searches that will return lists of Buildings in the database. If the Client is trying to
+   * see a list of Buildings, this method will be called
    *
-   * @return List of all buildings sorted alphabetically in JSON format
+   * @param searchType
+   *         Type of list the client wants to see. Can be all Buildings, searching by name Start, or searching by
+   *         abbreviation start
+   * @param param
+   *         What the client is searching with. Not a required parameter for this method to work.
+   *
+   * @return A list of Buildings that meet what the client wants. Or null if they somehow search something they
+   * shouldn't be able to search.
    */
-  @GetMapping(path = "/search/all") // should be alphabetical
+  @GetMapping(path = "/search/lists/{searchType}")
   public @ResponseBody
-  Iterable<Building> getAllBuildings()
+  Iterable<Building> getBuildingLists(@PathVariable String searchType, @RequestParam(required = false) String param)
   {
-    // use Java code names for HttpRequests. Not MySQL names
-    return buildingRepository.findAll(new Sort(Sort.Direction.ASC, "buildingName"));
+    switch (searchType)
+    {
+      case "all":
+        return buildingRepository.findAll(new Sort(Sort.Direction.ASC, "buildingName"));
+      case "nameStartsWith":
+        return buildingRepository.findAllByBuildingNameStartingWith(param);
+      case "abbreviationStartsWith":
+        return buildingRepository.findAllByAbbreviationStartingWith(param);
+      default:
+        return null;
+    }
   }
 
   /**
-   * Find the building with the given name
+   * Mapping for the different searches that will return a single Building in the database. If the Client is trying to
+   * find a single Building, this method will be called
    *
-   * @param name
-   *         Name of building to look for
+   * @param searchType
+   *         How the client wants to find their specific Building.
+   * @param param1
+   *         Parameter to refine search. This is parameter is required otherwise the Backend does not know what to
+   *         search for
+   * @param param2
+   *         Parameter used when search by location. This is for Longitude. This parameter is optional
    *
-   * @return JSON data of building
+   * @return A single Building that meets the parameters the Client entered
    */
-  @GetMapping(path = "/search/name")
+  @GetMapping(path = "/search/specific/{searchType}")
   public @ResponseBody
-  Building getBuildingByName(@RequestParam String name)
+  Building getBuildingBy(@PathVariable String searchType, @RequestParam Object param1,
+                         @RequestParam(required = false) Object param2)
   {
-    return buildingRepository.findByBuildingName(name);
-  }
-
-  /**
-   * Find buildings with names that start with given parameter
-   *
-   * @param startsWith
-   *         Characters buildings could start with
-   *
-   * @return List of Building data in JSON format that meet parameters
-   */
-  @GetMapping(path = "/search/nameStartsWith")
-  public @ResponseBody
-  Iterable<Building> getBuildingsStartingWith(@RequestParam String startsWith)
-  {
-    return buildingRepository.findAllByBuildingNameStartingWith(startsWith);
-  }
-
-  /**
-   * Find Building with given abbreviation
-   *
-   * @param abbreviation
-   *         Building abbreviation to search for
-   *
-   * @return Building data in JSON format of Building with given abbreviation
-   */
-  @GetMapping("/search/abbreviation")
-  public @ResponseBody
-  Building getBuildingByAbbrev(@RequestParam String abbreviation)
-  {
-    return buildingRepository.findByAbbreviation(abbreviation);
-  }
-
-  /**
-   * Find List of buildings whose abbreviations start with given text
-   *
-   * @param startsWith
-   *         text abbreviations are to start with
-   *
-   * @return List of Building data in JSON format of Buildings with start of given abbreviation
-   */
-  @GetMapping(path = "/search/abbreviationStartsWith")
-  public @ResponseBody
-  Iterable<Building> getBuildingsByAddressStartingWith(@RequestParam String startsWith)
-  {
-    return buildingRepository.findAllByAbbreviationStartingWith(startsWith);
-  }
-
-  /**
-   * Get building that has given address
-   *
-   * @param address
-   *         Address to look up
-   *
-   * @return data in JSON format of Building with given address
-   */
-  @GetMapping(path = "/search/address")
-  public @ResponseBody
-  Building getBuildingByAddress(@RequestParam String address)
-  {
-    return buildingRepository.findByAddress(address);
+    switch (searchType)
+    {
+      case "name":
+        return buildingRepository.findByBuildingName((String) param1);
+      case "abbreviation":
+        return buildingRepository.findByAbbreviation((String) param1);
+      case "address":
+        return buildingRepository.findByAddress((String) param1);
+      case "location":
+        return buildingRepository.findByLatitAndLongit((Double) param1, (Double) param2);
+      default:
+        return null;
+    }
   }
 
   /**
    * If A Student clicks on a Building and wants to know information about it, we know that the building exists, so we
-   * will use this method to get the information about the Building
+   * will use this method to get the information about the Building.
+   * <p>
+   * Has to be separate because searching by ID returns an Optional List of Buildings
    *
    * @param id
    *         id of Building that was clicked on
@@ -181,20 +159,112 @@ public class BuildingController
     return buildingRepository.findById(id);
   }
 
-  /**
-   * Return a Building's information if given it's exact latitude and longitude
-   *
-   * @param latit
-   *         latitude of building
-   * @param longit
-   *         longitude of building
-   *
-   * @return data in JSON format about Building at given coordinates
-   */
-  @GetMapping(path = "/search/location")
-  public @ResponseBody
-  Building getBuildingByLocation(@RequestParam Double latit, @RequestParam Double longit)
-  {
-    return buildingRepository.findByLatitAndLongit(latit, longit);
-  }
+  /*
+   * Below are old mappings that have been combined into 2 methods instead of many. The combination will make
+   * it easier to add onto if needed at a later date.
+   * */
+//  /**
+//   * Get a list of all buildings in the database
+//   *
+//   * @return List of all buildings sorted alphabetically in JSON format
+//   */
+//  @GetMapping(path = "/search/all") // should be alphabetical
+//  public @ResponseBody
+//  Iterable<Building> getAllBuildings()
+//  {
+//    // use Java code names for HttpRequests. Not MySQL names
+//    return buildingRepository.findAll(new Sort(Sort.Direction.ASC, "buildingName"));
+//  }
+//
+//  /**
+//   * Find the building with the given name
+//   *
+//   * @param name
+//   *         Name of building to look for
+//   *
+//   * @return JSON data of building
+//   */
+//  @GetMapping(path = "/search/name")
+//  public @ResponseBody
+//  Building getBuildingByName(@RequestParam String name)
+//  {
+//    return buildingRepository.findByBuildingName(name);
+//  }
+//
+//  /**
+//   * Find buildings with names that start with given parameter
+//   *
+//   * @param startsWith
+//   *         Characters buildings could start with
+//   *
+//   * @return List of Building data in JSON format that meet parameters
+//   */
+//  @GetMapping(path = "/search/nameStartsWith")
+//  public @ResponseBody
+//  Iterable<Building> getBuildingsStartingWith(@RequestParam String startsWith)
+//  {
+//    return buildingRepository.findAllByBuildingNameStartingWith(startsWith);
+//  }
+//
+//  /**
+//   * Find Building with given abbreviation
+//   *
+//   * @param abbreviation
+//   *         Building abbreviation to search for
+//   *
+//   * @return Building data in JSON format of Building with given abbreviation
+//   */
+//  @GetMapping("/search/abbreviation")
+//  public @ResponseBody
+//  Building getBuildingByAbbrev(@RequestParam String abbreviation)
+//  {
+//    return buildingRepository.findByAbbreviation(abbreviation);
+//  }
+//
+//  /**
+//   * Find List of buildings whose abbreviations start with given text
+//   *
+//   * @param startsWith
+//   *         text abbreviations are to start with
+//   *
+//   * @return List of Building data in JSON format of Buildings with start of given abbreviation
+//   */
+//  @GetMapping(path = "/search/abbreviationStartsWith")
+//  public @ResponseBody
+//  Iterable<Building> getBuildingsByAddressStartingWith(@RequestParam String startsWith)
+//  {
+//    return buildingRepository.findAllByAbbreviationStartingWith(startsWith);
+//  }
+//
+//  /**
+//   * Get building that has given address
+//   *
+//   * @param address
+//   *         Address to look up
+//   *
+//   * @return data in JSON format of Building with given address
+//   */
+//  @GetMapping(path = "/search/address")
+//  public @ResponseBody
+//  Building getBuildingByAddress(@RequestParam String address)
+//  {
+//    return buildingRepository.findByAddress(address);
+//  }
+//
+//  /**
+//   * Return a Building's information if given it's exact latitude and longitude
+//   *
+//   * @param latit
+//   *         latitude of building
+//   * @param longit
+//   *         longitude of building
+//   *
+//   * @return data in JSON format about Building at given coordinates
+//   */
+//  @GetMapping(path = "/search/location")
+//  public @ResponseBody
+//  Building getBuildingByLocation(@RequestParam Double latit, @RequestParam Double longit)
+//  {
+//    return buildingRepository.findByLatitAndLongit(latit, longit);
+//  }
 }
--- BackEnd/database/src/main/java/com/ss4/opencampus/backend/database/students/StudentController.java
+++ BackEnd/database/src/main/java/com/ss4/opencampus/backend/database/students/StudentController.java
@@ -34,7 +34,7 @@ public class StudentController
    */
   @PostMapping(path = "/add")
   public @ResponseBody
-  Map<String, String> addNewStudent(@RequestBody Student student)
+  Map<String, Boolean> addNewStudent(@RequestBody Student student)
   {
     try
     {
@@ -42,89 +42,75 @@ public class StudentController
     }
     catch (Exception e)
     {
-      return Collections.singletonMap("response", "Failed to add student to Students table");
+      return Collections.singletonMap("response", false);
     }
-    return Collections.singletonMap("response", "Added student to Students table");
+    return Collections.singletonMap("response", true);
   }
 
   /**
-   * Return list of all Students in Students table
+   * Mapping for the different searches that will return lists of Students in the database. If the Client is trying to
+   * see a list of Students, this method will be called
    *
-   * @return list of all Students in Students table in JSON format
+   * @param searchType
+   *         Type of list the client wants to see. Can be all Students, searching by name Start, or searching by
+   *         abbreviation start
+   * @param param1
+   *         What the client is searching with. Not a required parameter for this method to work.
+   * @param param2
+   *         Another optional parameter to help specific what the Client wants in the returned list
+   *
+   * @return A list of Students that meet what the client wants. Or null if they somehow search something they shouldn't
+   * be able to search.
    */
-  @GetMapping(path = "/search/all")
+  @GetMapping(path = "/search/lists/{searchType}")
   public @ResponseBody
-  Iterable<Student> getAllStudents()
+  Iterable<Student> getStudentLists(@PathVariable String searchType, @RequestParam(required = false) String param1, @RequestParam(required = false) String param2)
   {
-    return studentRepository.findAll();
+    switch (searchType)
+    {
+      case "all":
+        return studentRepository.findAll();
+      case "firstName":
+        return studentRepository.findAllByFirstName(param1);
+      case "lastName":
+        return studentRepository.findAllByLastName(param1);
+      case "firstNameAndLastName":
+        return studentRepository.findAllByFirstNameAndLastName(param1, param2);
+      default:
+        return null;
+    }
   }
 
   /**
-   * Gives Client information about the Student with the given userName. userName's are unique, so only one user will be
-   * returned.
+   * Mapping for the different searches that will return a single Student in the database. If the Client is trying to
+   * find a single Student, this method will be called. Currently, the only way to search for a single Student is by
+   * userName. Can easily be added onto.
    *
-   * @param userName
-   *         userName to look for
+   * @param searchType
+   *         How the client wants to find their specific Student.
+   * @param param
+   *         Parameter to refine search. This is parameter is required otherwise the Backend does not know what to
+   *         search for
    *
-   * @return Information about Student with given userName in JSON format
+   * @return A Student with the matching info the was entered. Will be null if error.
    */
-  @GetMapping(path = "/search/userName")
+  @GetMapping(path = "/search/specific/{searchType}")
   public @ResponseBody
-  Student getStudentByUsername(@RequestParam String userName)
+  Student getStudentBy(@PathVariable String searchType, @RequestParam Object param)
   {
-    return studentRepository.findByUserName(userName);
-  }
-
-  /**
-   * Gives Client information about Students with entered firstName.
-   *
-   * @param firstName
-   *         firstName to search for
-   *
-   * @return List of Students in JSON format with given firstName
-   */
-  @GetMapping(path = "/search/firstName")
-  public @ResponseBody
-  Iterable<Student> getStudentsByFirstName(@RequestParam String firstName)
-  {
-    return studentRepository.findAllByFirstName(firstName);
-  }
-
-  /**
-   * Gives Client information about Students with entered lastName.
-   *
-   * @param lastName
-   *         lastName to search for
-   *
-   * @return List of Students in JSON format with given lastName
-   */
-  @GetMapping(path = "/search/lastName")
-  public @ResponseBody
-  Iterable<Student> getStudentsByLastName(@RequestParam String lastName)
-  {
-    return studentRepository.findAllByLastName(lastName);
-  }
-
-  /**
-   * Gives Client information about Students with entered firstName and lastName.
-   *
-   * @param firstName
-   *         firstName to look for
-   * @param lastName
-   *         lastName to look for
-   *
-   * @return List of Students with the same first and last names in JSON format
-   */
-  @GetMapping(path = "/search/firstNameAndLastName")
-  public @ResponseBody
-  Iterable<Student> getStudentsByFirstAndLastName(@RequestParam String firstName, @RequestParam String lastName)
-  {
-    return studentRepository.findAllByFirstNameAndLastName(firstName, lastName);
+    // can be replaced with switch if more cases are added.
+    if ("userName".equals(searchType))
+    {
+      return studentRepository.findByUserName((String) param);
+    }
+    return null;
   }
 
   /**
    * Will rarely be used. If a Student clicks on an existing Student that 100% exists, this will be used to grab the
    * information about that Student.
+   * <p>
+   * Has to be separate because to search by ID you need to return an Optional list of Students.
    *
    * @param id
    *         id of Student that was clicked
@@ -137,5 +123,86 @@ public class StudentController
   {
     return studentRepository.findById(id);
   }
+
+  /*
+   * Below are old mappings that have been combined into 2 methods instead of many. The combination will make
+   * it easier to add onto if needed at a later date.
+   * */
+//
+//  /**
+//   * Return list of all Students in Students table
+//   *
+//   * @return list of all Students in Students table in JSON format
+//   */
+//  @GetMapping(path = "/search/all")
+//  public @ResponseBody
+//  Iterable<Student> getAllStudents()
+//  {
+//    return studentRepository.findAll();
+//  }
+//
+//  /**
+//   * Gives Client information about the Student with the given userName. userName's are unique, so only one user will be
+//   * returned.
+//   *
+//   * @param userName
+//   *         userName to look for
+//   *
+//   * @return Information about Student with given userName in JSON format
+//   */
+//  @GetMapping(path = "/search/userName")
+//  public @ResponseBody
+//  Student getStudentByUsername(@RequestParam String userName)
+//  {
+//    return studentRepository.findByUserName(userName);
+//  }
+//
+//  /**
+//   * Gives Client information about Students with entered firstName.
+//   *
+//   * @param firstName
+//   *         firstName to search for
+//   *
+//   * @return List of Students in JSON format with given firstName
+//   */
+//  @GetMapping(path = "/search/firstName")
+//  public @ResponseBody
+//  Iterable<Student> getStudentsByFirstName(@RequestParam String firstName)
+//  {
+//    return studentRepository.findAllByFirstName(firstName);
+//  }
+//
+//  /**
+//   * Gives Client information about Students with entered lastName.
+//   *
+//   * @param lastName
+//   *         lastName to search for
+//   *
+//   * @return List of Students in JSON format with given lastName
+//   */
+//  @GetMapping(path = "/search/lastName")
+//  public @ResponseBody
+//  Iterable<Student> getStudentsByLastName(@RequestParam String lastName)
+//  {
+//    return studentRepository.findAllByLastName(lastName);
+//  }
+//
+//  /**
+//   * Gives Client information about Students with entered firstName and lastName.
+//   *
+//   * @param firstName
+//   *         firstName to look for
+//   * @param lastName
+//   *         lastName to look for
+//   *
+//   * @return List of Students with the same first and last names in JSON format
+//   */
+//  @GetMapping(path = "/search/firstNameAndLastName")
+//  public @ResponseBody
+//  Iterable<Student> getStudentsByFirstAndLastName(@RequestParam String firstName, @RequestParam String lastName)
+//  {
+//    return studentRepository.findAllByFirstNameAndLastName(firstName, lastName);
+//  }
+//
 
 }
