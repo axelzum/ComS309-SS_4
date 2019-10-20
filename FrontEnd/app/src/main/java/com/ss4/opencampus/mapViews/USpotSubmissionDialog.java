32133c20563a1f33f7cda3092d3990ee1b88149c
bsantema <bsantema@iastate.edu>
2019-10-20 16:00:36 -0500

    Added rating bar for USpot submission.

--- FrontEnd/app/src/main/java/com/ss4/opencampus/mapViews/USpotSubmissionDialog.java
+++ FrontEnd/app/src/main/java/com/ss4/opencampus/mapViews/USpotSubmissionDialog.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -16,7 +18,9 @@ import com.ss4.opencampus.R;
 public class USpotSubmissionDialog extends DialogFragment{
 
     private TextView mActionCancel, mActionSubmit;
+    private EditText title;
     private Spinner category;
+    private RatingBar rating;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +28,10 @@ public class USpotSubmissionDialog extends DialogFragment{
 
         mActionCancel = view.findViewById(R.id.action_cancel);
         mActionSubmit = view.findViewById(R.id.action_submit);
+        title = view.findViewById(R.id.name_text);
         category = view.findViewById(R.id.category_dropdown);
+        rating = view.findViewById(R.id.ratingbar);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.category_array, R.layout.support_simple_spinner_dropdown_item);
         category.setAdapter(adapter);
         mActionCancel.setOnClickListener(new View.OnClickListener() {
@@ -33,7 +40,16 @@ public class USpotSubmissionDialog extends DialogFragment{
                 getDialog().dismiss();
             }
         });
-
+        mActionSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // POST
+                //title.getText().toString();
+                //category.getSelectedItem().toString();
+                //rating.getRating();
+                getDialog().dismiss();
+            }
+        });
         return view;
     }
 
