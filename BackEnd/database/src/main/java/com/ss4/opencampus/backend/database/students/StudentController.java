a983eb946501826d728fc63924aee843d864cdb5
Axel Zumwalt <axelzum@iastate.edu>
2019-10-21 19:04:06 -0500

    [SS4-24] Return same response on add success

--- BackEnd/database/src/main/java/com/ss4/opencampus/backend/database/students/StudentController.java
+++ BackEnd/database/src/main/java/com/ss4/opencampus/backend/database/students/StudentController.java
@@ -38,27 +38,27 @@ public class StudentController
   public @ResponseBody
   Map<String, Boolean> addNewStudent(@RequestBody Student student)
   {
+    boolean dupUsername = false;
+    boolean dupEmail = false;
     try
     {
       studentRepository.save(student);
     }
     catch (Exception e)
     {
-      boolean dupUsername = false;
-      boolean dupEmail = false;
       if (e.getMessage().equals("could not execute statement; SQL [n/a]; constraint [Username_UNIQUE]; nested exception is org.hibernate.exception.ConstraintViolationException: could not execute statement")) {
         dupUsername = true;
       }
       else if (e.getMessage().equals("could not execute statement; SQL [n/a]; constraint [Email_UNIQUE]; nested exception is org.hibernate.exception.ConstraintViolationException: could not execute statement")) {
         dupEmail = true;
       }
-      Map<String, Boolean> errorResponse = new HashMap<String, Boolean>();
-      errorResponse.put("duplicateUsername", dupUsername);
-      errorResponse.put("duplicateEmail", dupEmail);
 
-      return errorResponse;
     }
-    return Collections.singletonMap("addedUser", true);
+    Map<String, Boolean> errorResponse = new HashMap<String, Boolean>();
+    errorResponse.put("duplicateUsername", dupUsername);
+    errorResponse.put("duplicateEmail", dupEmail);
+
+    return errorResponse;
   }
 
   /**
