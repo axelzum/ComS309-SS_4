176d3d4cb6770956d211c57a66d0f9605b984f9a
Willis Knox <wpknox@iastate.edu>
2019-11-18 19:45:15 -0600

    [SS4-53] Make Endpoints for Routes

--- BackEnd/database/src/main/java/com/ss4/opencampus/backend/database/students/routes/Route.java
+++ BackEnd/database/src/main/java/com/ss4/opencampus/backend/database/students/routes/Route.java
@@ -7,6 +7,9 @@ import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
+/**
+ * @author Willis Knox
+ */
 @Entity
 @Table(name = "Routes")
 public class Route
@@ -14,6 +17,21 @@ public class Route
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
+
+  @Column(name = "Route_Name")
+  private String rtName;
+
+  @Column(name = "Start_Lat")
+  private Double originLat;
+
+  @Column(name = "Start_Lng")
+  private Double originLng;
+
+  @Column(name = "End_Lat")
+  private Double destLat;
+
+  @Column(name = "End_Lng")
+  private Double destLng;
 
   /**
    * Foreign key to the Student Table. Links the Student_ID to this Route. If the Student is deleted, so will
@@ -24,5 +42,79 @@ public class Route
   @OnDelete(action = OnDeleteAction.CASCADE)
   @JsonIgnore
   private Student student;
-  //more to come
+
+  public Route()
+  {
+
+  }
+
+  public Integer getId()
+  {
+    return id;
+  }
+
+  public void setId(Integer id)
+  {
+    this.id = id;
+  }
+
+  public String getRtName()
+  {
+    return rtName;
+  }
+
+  public void setRtName(String rtName)
+  {
+    this.rtName = rtName;
+  }
+
+  public Double getOriginLat()
+  {
+    return originLat;
+  }
+
+  public void setOriginLat(Double originLat)
+  {
+    this.originLat = originLat;
+  }
+
+  public Double getOriginLng()
+  {
+    return originLng;
+  }
+
+  public void setOriginLng(Double originLng)
+  {
+    this.originLng = originLng;
+  }
+
+  public Double getDestLat()
+  {
+    return destLat;
+  }
+
+  public void setDestLat(Double destLat)
+  {
+    this.destLat = destLat;
+  }
+
+  public Double getDestLng()
+  {
+    return destLng;
+  }
+
+  public void setDestLng(Double destLng)
+  {
+    this.destLng = destLng;
+  }
+
+  public Student getStudent()
+  {
+    return student;
+  }
+
+  public void setStudent(Student student)
+  {
+    this.student = student;
+  }
 }
