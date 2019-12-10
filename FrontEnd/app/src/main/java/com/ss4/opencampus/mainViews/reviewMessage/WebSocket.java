9aecf2eed5c3faa4b1a11e2e1af09fdbbf498836
Axel Zumwalt <axelzum@iastate.edu>
2019-12-10 01:33:18 -0600

    [SS4-65] WebSocket GET request

--- FrontEnd/app/src/main/java/com/ss4/opencampus/mainViews/reviewMessage/WebSocket.java
+++ FrontEnd/app/src/main/java/com/ss4/opencampus/mainViews/reviewMessage/WebSocket.java
@@ -2,6 +2,7 @@ package com.ss4.opencampus.mainViews.reviewMessage;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Base64;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +14,11 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.ss4.opencampus.dataViews.uspots.SingleUSpotActivity;
+import com.ss4.opencampus.dataViews.uspots.USpot;
+import com.ss4.opencampus.dataViews.uspots.USpotListActivity;
 import com.ss4.opencampus.mainViews.DashboardActivity;
+import com.ss4.opencampus.mainViews.NetworkingUtils;
 import com.ss4.opencampus.mainViews.login.LoginPreferenceUtils;
 
 import org.java_websocket.client.WebSocketClient;
@@ -33,12 +38,6 @@ import java.util.ArrayList;
  * Web socket for recieveing notifications for USpot comments.
  */
 public class WebSocket {
-    public static final String TAG = "tag";
-
-    private Button btnSend;
-    private Button btnDash;
-    private TextView txtChat;
-    private EditText editMsg;
 
     private static WebSocketClient cc;
 
@@ -59,65 +58,55 @@ public class WebSocket {
             cc = new WebSocketClient(new URI(url), (Draft) draft[0]) {
                 @Override
                 public void onOpen(ServerHandshake handshakedata) {
-                    Log.d("THING HAPPEN", "opened");
+                    Log.d("WEB SOCKET OPENED", "");
                 }
 
                 @Override
                 public void onMessage(String message) {
-                    Log.d("TO NOTIFY USER :", message);
-                    int USpotId = Integer.parseInt(message);
 
-                    RequestQueue queue = Volley.newRequestQueue(context);
+                    int USpotId = Integer.parseInt(message);
                     String url = "http://coms-309-ss-4.misc.iastate.edu:8080/uspots/search/id/" + Integer.toString(USpotId);
 
-                    JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                            new Response.Listener<JSONObject>() {    // Reads in JSON data for the uspots from the server
-                                /**
-                                 * Makes a GET Request to Backend to get the USpot with the given ID in the database.
-                                 * @param response JSON format of information from Backend
-                                 */
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                    try {
-                                        int usID = response.getInt("usID");
-                                        String usName = response.getString("usName");
+                    Response.Listener<JSONObject> listenerResponse = new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                int usID = response.getInt("usID");
+                                String usName = response.getString("usName");
 
-                                        ReviewMessage reviewMessage = new ReviewMessage(usID, usName, false);
+                                ReviewMessage reviewMessage = new ReviewMessage(usID, usName, false);
 
-                                        ArrayList<ReviewMessage> messageList = (ArrayList<ReviewMessage>) ReviewMessagePreferenceUtils.getReviewMessageList(context);
-                                        if (messageList == null) {
-                                            messageList = new ArrayList<ReviewMessage>();
-                                        }
-                                        messageList.add(reviewMessage);
-
-                                        ReviewMessagePreferenceUtils.addReviewMessageList(messageList, context);
-
-                                        Intent intent = new Intent(context, DashboardActivity.class);
-                                        context.startActivity(intent);
-
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
+                                ArrayList<ReviewMessage> messageList = (ArrayList<ReviewMessage>) ReviewMessagePreferenceUtils.getReviewMessageList(context);
+                                if (messageList == null) {
+                                    messageList = new ArrayList<ReviewMessage>();
                                 }
-                            }, new Response.ErrorListener() {
-                        /**
-                         * Prints an the error if something goes wrong
-                         * @param error Type of error that occurred
-                         */
+                                messageList.add(reviewMessage);
+
+                                ReviewMessagePreferenceUtils.addReviewMessageList(messageList, context);
+
+                                //TODO Not a perfect solution for refreshing Dashboard
+                                Intent intent = new Intent(context, DashboardActivity.class);
+                                context.startActivity(intent);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+
+                    Response.ErrorListener listenerError = new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
                             error.printStackTrace();
                         }
-                    });
-                    //Set the tag on the request
-                    jsonRequest.setTag(TAG);
-                    // Add the request to the RequestQueue.
-                    queue.add(jsonRequest);
+                    };
+
+                    NetworkingUtils.sendGetRequest(context, url, listenerResponse, listenerError);
                 }
 
                 @Override
                 public void onClose(int code, String reason, boolean remote) {
-                    Log.d("THING HAPPEN", "closed" + reason);
+                    Log.d("WEB SOCKET CLOSED", reason);
                 }
 
                 @Override
